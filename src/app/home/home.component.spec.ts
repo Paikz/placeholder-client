91dbcf80267daa2fcacbb4dd0b24c07f133c2038
paikz <philipesmailzade@gmail.com>
2017-12-23 14:11:39 +0100

    fixed test in homecomponent

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { AuthService } from '../services/auth.service';
 
 describe('HomeComponent', () => {
@@ -10,7 +11,7 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule ],
-      declarations: [ HomeComponent ],
+      declarations: [ HomeComponent, NavbarComponent ],
       providers: [AuthService]
     })
     .compileComponents();
